1d9efd96db50ecf6819ddf90fbc4b7bee7946f51
Romain Dalichamp <romain.dalichamp@gmail.com>
2026-03-16 18:57:20 -0400

    change-explosion-color

--- src/scenes/entities/player/fireball/fireball.gd
+++ src/scenes/entities/player/fireball/fireball.gd
@@ -19,6 +19,7 @@ func _ready() -> void:
 	body_entered.connect(die)
 	area_entered.connect(die)
 	sprite.modulate = Color(0.5, 0, 1)  # boules de feu violettes !
+	spark_particles.modulate = Color(0, 1, 0)
 
 
 func _physics_process(delta: float) -> void:
