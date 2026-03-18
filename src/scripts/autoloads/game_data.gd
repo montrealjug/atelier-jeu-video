b2088316fb3252116f48e62506bf911b14e4e416
Romain Dalichamp <romain.dalichamp@gmail.com>
2026-03-18 00:48:45 -0400

    merge with master changes

--- src/scripts/autoloads/game_data.gd
+++ src/scripts/autoloads/game_data.gd
@@ -7,8 +7,6 @@ const ENEMY_GROUP := "enemy"
 
 var player: Player
 var camera: PlayerCamera
-## Total number of ennemies killed
-var score: int = 0
 ## The node that contains all elements that should be y-sorted
 var y_sorted_elements: Node2D
 ## The area where enemies are allowed to spawn
@@ -19,6 +17,7 @@ var waves_data: Array[WaveData]
 var game_root: Node2D
 
 ## -- score variables here --
+var score: int = 0
 
 ## -- survival variables here --
 
